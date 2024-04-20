ed40f77310989b4f95e2efcc41ff60ef738044a7
Sonu Gupta <sonu72752@gmail.com>
2024-04-20 18:10:14 -0500

    backend api

---
+++ MERN/backend/routes/recipes.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all recipes'})
+})
+
+// GET a single recipe
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single recipe'})
+})
+
+// POST a new recipe
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new recipe'})
+})
+
+// DELETE a recipe
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'Delete a recipe'})
+})
+
+// UPDATE a workout
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a recipe'})
+})
+
+module.exports = router
